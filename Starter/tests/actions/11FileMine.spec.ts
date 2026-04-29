eb7ec823a6d055da0ac2a975fe8d9d063f283ded
Krasimir Petkov <krasimir5kov5kov@gmail.com>
2026-04-29 12:04:32 +0300

    New exercises files from today 29.04.2026

--- Starter/tests/actions/11FileMine.spec.ts
+++ Starter/tests/actions/11FileMine.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test";
-import path from "path";
+import {join} from "path";
 
 test("Upload a file ", async ({ page }) => {
   await page.goto("Files.html");
@@ -21,4 +21,23 @@ test("Upload a file ", async ({ page }) => {
       buffer: Buffer.from("This is the content of file 2"),
     },
   ]);
+
+  await expect(page.locator('#fileListContainer')).toContainText(fileName1);
+    await expect(page.locator('#fileListContainer')).toContainText(fileName2);
+});
+
+test("Download a file", async ({ page }) => {
+    await page.goto("Files.html");
+
+    const downloadPromise = page.waitForEvent("download");
+
+    await page.getByRole('button',{name: "Download document"}).click();
+
+    const download = await downloadPromise;
+
+    const path = join(__dirname, '..','..','test-results','files', download.suggestedFilename());
+
+    await download.saveAs(path);
+
+
 });
